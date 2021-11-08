aecb9d346dd151561f6df20eac6c5e89c2cf12c5
Anusha V <77875582+AnushaV-2002@users.noreply.github.com>
2021-11-08 17:05:06 +0530

    updated

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,22 @@
+var countDownDate = new Date("NOV 27, 2021 05:00:00").getTime();
+var day=document.getElementById("day");
+var hr=document.getElementById("hr");
+var min=document.getElementById("min");
+var sec=document.getElementById("sec");
+
+var myfunc = setInterval(function() {
+    // code goes here
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+    
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    day.innerHTML=days;
+    hr.innerHTML=hours;
+    min.innerHTML=minutes;
+    sec.innerHTML=seconds;
+    
+}, 1000)
 
